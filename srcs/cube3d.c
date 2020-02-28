28a031d04e88bb5a4db10fffa6a29a094d646033
Guillaume OZSERTTAS <gozsertt@e2r10p11.42.fr>
2020-02-28 12:57:59 +0100

    First render

--- srcs/cube3d.c
+++ srcs/cube3d.c
@@ -6,7 +6,7 @@
 /*   By: gozsertt <gozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 13:25:29 by gozsertt          #+#    #+#             */
-/*   Updated: 2020/02/26 13:44:29 by gozsertt         ###   ########.fr       */
+/*   Updated: 2020/02/28 12:57:27 by gozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,35 +45,29 @@ y = sin(alpha)
 
 int handle_key(int key, void *param)
 {
-	static int move_speed = 3;
-	// t_vector2 *pos;
-	// t_vector2 *pos2;
+	static float move_speed = 0.5;
+	static float rotation_speed = 10;
+	t_player *player;
 
-	// pos = ((void **)(param))[2];
-	// pos2 = ((void **)(param))[3];
-	(void)param;
+	player = (t_player*)param;
 	if (key == ESC_KEY)
 		close_application();
-	if (key == NK4_KEY && move_speed > 1)
-		move_speed--;
-	if (key == NK6_KEY && move_speed < 10)
-		move_speed++;
-	// if (key == A_KEY)
-	// 	pos->x -= move_speed;
-	// if (key == D_KEY)
-	// 	pos->x += move_speed;
-	// if (key == W_KEY)
-	// 	pos->y -= move_speed;
-	// if (key == S_KEY)
-	// 	pos->y += move_speed;
-	// if (key == LEFT_KEY)
-	// 	pos2->x -= move_speed;
-	// if (key == RIGHT_KEY)
-	// 	pos2->x += move_speed;
-	// if (key == UP_KEY)
-	// 	pos2->y -= move_speed;
-	// if (key == DOWN_KEY)
-	// 	pos2->y += move_speed;
+	// if (key == NK4_KEY && move_speed > 1)
+	// 	move_speed--;
+	// if (key == NK6_KEY && move_speed < 10)
+	// 	move_speed++;
+	if (key == A_KEY)
+		player->pos.x -= move_speed;
+	if (key == D_KEY)
+		player->pos.x += move_speed;
+	if (key == W_KEY)
+		player->pos.y -= move_speed;
+	if (key == S_KEY)
+		player->pos.y += move_speed;
+	if (key == LEFT_KEY)
+		player->angle -= rotation_speed;
+	if (key == RIGHT_KEY)
+		player->angle += rotation_speed;
 	return (0);
 }
 
@@ -91,7 +85,6 @@ int test_render_app(void *param) // -> void **param
 	pos2 = ((void **)(param))[3];
 
 	clear_application(create_color(50, 50, 50, 255));
-	debug;
 	draw_rectangle(*pos, create_vector2(200, 200), *color);
 	draw_rectangle(*pos2, create_vector2(200, 200), *color2);
 
@@ -104,7 +97,7 @@ t_vector2	get_contact_pos(t_vector2 pos, t_vector2 target)
 {
 	t_vector2 tmp_pos;
 	t_vector2 tmp_target;
-	//t_vector2 result;
+	// t_vector2 result;
 
 	tmp_pos = substract_vector2_to_vector2(pos, pos);
 	tmp_target = substract_vector2_to_vector2(target, pos);
@@ -124,8 +117,51 @@ void 		*calc_detection(t_map *map, t_vector2 pos)
 	return (NULL);
 }
 
+t_vector2	cut_aff_x(t_vector2 player_actual, t_vector2 delta, float angle)
+{
+	t_vector2 result;
+	
+	if (delta.x >= 0)
+	{
+		result.x = ((int)player_actual.x + 1) - player_actual.x;
+		result.y = result.x * tan(angle);
+	}
+	else
+	{
+		result.x = (int)player_actual.x - player_actual.x;
+		result.y = result.x * tan(angle);
+	}
+	return (result);
+}
+
+t_vector2	cut_aff_y(t_vector2 player_actual, t_vector2 delta, float angle)
+{
+	t_vector2 result;
+
+	if (delta.y >= 0)
+	{
+		result.y = (int)player_actual.y + 1 - player_actual.y;
+		result.x = result.y / tan(angle);
+	}
+	else
+	{
+		result.y = (int)player_actual.y - player_actual.y;
+		result.x = result.y / tan(angle);
+	}
+	return (result);
+}
+
+float		calc_dist(t_vector2 p1, t_vector2 p2)
+{
+	float result;
+
+	result = pow((p1.x - p2.x), 2) + pow((p1.y - p2.y), 2);
+	return (result);
+}
+
 t_vector2 cast_ray(t_map *map, t_vector2 actual, float angle)
 {
+	t_vector2	cut_tab[2];
 	t_vector2	delta;
 	void		*hit;
 	float		radian;
@@ -133,15 +169,46 @@ t_vector2 cast_ray(t_map *map, t_vector2 actual, float angle)
 	radian = degree_to_radian(angle);
 	delta = create_vector2(cos(radian), sin(radian));
 	hit = NULL;
-	while (	hit == NULL &&
-			actual.x >= 0 && actual.x < map->size.x &&
-			actual.y >= 0 && actual.y < map->size.y)
+	cut_tab[0] = add_vector2_to_vector2(actual, cut_aff_x(actual, delta, radian));
+	cut_tab[1] = add_vector2_to_vector2(actual, cut_aff_y(actual, delta, radian));
+
+	while (hit == NULL)
 	{
-		actual = add_vector2_to_vector2(actual, delta);
-		hit = calc_detection(map, actual);
+		// actual = add_vector2_to_vector2(actual, delta);
+
+		if (calc_dist(actual, cut_tab[0]) < calc_dist(actual, cut_tab[1]))
+		{
+			hit = calc_detection(map, cut_tab[0]);
+			if (hit == NULL)
+			{
+				cut_tab[0].x += (delta.x >= 0) ? 1 : -1;
+				cut_tab[0].y += (delta.x >= 0) ? 1 * tan(radian) : -1 * tan(radian);
+			}
+		}
+		else
+		{
+			hit = calc_detection(map, cut_tab[1]);
+			if (hit == NULL)
+			{
+				cut_tab[1].y += (delta.y >= 0) ? 1 : -1;
+				cut_tab[1].x += (delta.y >= 0) ? 1 / tan(radian) : -1 / tan(radian);
+			}
+		}
 	}
-	printf("Wall hit at angle %f = %d / %d\n", angle, (int)(actual.x), (int)(actual.y));
-	return (actual);
+	if (calc_dist(actual, cut_tab[0]) < calc_dist(actual, cut_tab[1]))
+		return (cut_tab[0]);
+	return (cut_tab[1]);
+}
+
+void	draw_wall(t_vector2 impact, t_vector2 actual_pos, float actual_angle, int x)
+{
+	float dist;
+	float height;
+
+	dist = sqrt(calc_dist(actual_pos, impact));
+	//h = 40 | d = 3
+	height = ((0.40 * g_app->size.y * actual_angle) * 3) / dist;
+	draw_rectangle(create_vector2((g_app->size.x - x), (g_app->size.y - height) / 2), create_vector2(1, height), create_color(120, 120, 120, 255));
 }
 
 int draw_map(void *param)
@@ -165,6 +232,7 @@ sin -> on a o et h / cos -> on a a et h / tan -> on a o a
 x = cos(alpha)
 y = sin(alpha)
 */
+	t_vector2 impact;
 	float angle_begin;
 	float angle_delta;
 	float angle_actual;
@@ -173,12 +241,12 @@ y = sin(alpha)
 	angle_begin = engine->player->angle + FOV / 2;
 	angle_delta = FOV / g_app->size.x;
 
-	printf("---=====---\n");
 	i = 0;
 	while (i < g_app->size.x)
 	{
 		angle_actual = angle_begin - angle_delta * i;
-		cast_ray(engine->map, engine->player->pos, angle_actual);
+		impact = cast_ray(engine->map, engine->player->pos, angle_actual);
+		draw_wall(impact, engine->player->pos, angle_actual, i);
 		i++;
 	}
 	render_application();
@@ -189,7 +257,6 @@ int main(int argc, char **argv)
 {
 	t_game_engine	engine;
 	t_vector2		resolution;
-	void			*param[4];
 
 	if (argc != 2)
 		catch_error(MAIN_1);
@@ -198,14 +265,9 @@ int main(int argc, char **argv)
 	cube3d_parsing(&engine, argv[1], &resolution);
 	resize_application((int)resolution.x, (int)resolution.y);
 
-	// param[0] = malloc_color(255, 0, 0, 255);
-	// param[1] = malloc_color(0, 0, 255, 125);
-	// param[2] = malloc_vector2(400, 400);
-	// param[3] = malloc_vector2(400, 400);
-
 	// ft_printf("Player at %v / %d\n", engine.player->pos, (int)(engine.player->angle)); fix %v printf
 
-	add_interaction_to_application(KEYPRESS, &handle_key, param);
+	add_interaction_to_application(KEYPRESS, &handle_key, engine.player);
 	render_funct_application(&draw_map, &engine);
 
 	return (run_application());
